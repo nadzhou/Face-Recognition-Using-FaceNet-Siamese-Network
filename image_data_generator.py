32ad6db9b9dabb349c90c36aa10681c1d40fefc7
nadzhou <ahmaddnavid@gmail.com>
2021-02-16 16:32:56 +0500

    Refactored the face_recognizer code

--- image_data_generator.py
+++ image_data_generator.py
@@ -6,11 +6,7 @@ from imutils import face_utils
 from imutils.face_utils import FaceAligner
 
 
-print("hello")
-
 def main(): 
-	print("hello2")
-
 	detector = dlib.get_frontal_face_detector()
 	shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 	face_aligner = FaceAligner(shape_predictor, desiredFaceWidth=200)
